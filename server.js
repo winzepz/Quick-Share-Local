ccd796d26a84e5e2448a236b414fac01e0727a3e
winzepz <winzepz24@gmail.com>
2025-05-10 12:52:50 +0545

    update on port

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
